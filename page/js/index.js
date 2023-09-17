35026139079895d7be1e3ba49620eabdb2c0ac4d
Orangesoft <104307711+CoderOrangesoft@users.noreply.github.com>
2023-09-17 23:09:22 +0800

    Create index.js

---
+++ page/js/index.js
@@ -0,0 +1,20 @@
+/*
+  欢迎来到 index.js
+  这里是存放函数库的地方，任何人只能对他人的函数进行修订，如果要删除他人的函数，需要 blockjs 全体开发人员全体表决。
+  第一个函数为view()，以后的函数都要按这个写，修订时必须记录修订日期、谁修订的和修订了什么
+  修订完后记得在开发者一栏加上名字，非必要(如参数调整，用法调整)不修订函数说明和使用方法
+*/
+
+/*
+  view()
+  开发者:Orangesoft
+  函数说明:调用后使网页后端的记录的浏览量+1
+  使用方法:直接调用,无需参数
+  修订历史记录:null
+*/
+function view(){
+  let http_view = new XMLHttpRequest;
+  let url_view = "https://pickfish.repl.co/count";
+  http_view.open('GET',url_view);
+  http_view.send();
+}
